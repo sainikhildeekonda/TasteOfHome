689662b624c9ae0662f9abff8c49533909251669
Rajeshgurram163 <64857443+Rajeshgurram163@users.noreply.github.com>
2020-12-03 00:21:47 -0500

    description activity class working succesfully
    description activity class working succesfully

--- app/src/main/java/com/example/tasteofhome/Description.java
+++ app/src/main/java/com/example/tasteofhome/Description.java
@@ -1,8 +1,29 @@
 package com.example.tasteofhome;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 
-public class Description {
+public class Description extends AppCompatActivity {
 
+    private TextView description;
 
+    private int pos;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_description);
+
+        description=findViewById(R.id.description1);
+
+       String nikhil=getIntent().getStringExtra("description");
+        description.setText(nikhil);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
 }
